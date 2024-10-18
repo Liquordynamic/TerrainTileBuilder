45c47027497925827dbe93075cfc9810bd89e46f
YcSoku <dsssyc02@gmail.com>
2024-10-18 22:08:41 +0800

    Upload files

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include "RootDir.h"
+#include "gridxtyp.h"
+#include "scatdata.h"
+#include "surfgrid.h"
+#include "xpand.h"
+#include "nlohmann/json.hpp"
 
 int main() {
     std::cout << ROOT_DIR << std::endl;
